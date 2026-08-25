6894b4924f6f9c6924fda8ce1b12163df17cfb44
SmokingPuddle58 <89989565+SmokingPuddle58@users.noreply.github.com>
2026-08-25 23:27:35 +0800

    chore: Added content collection format

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+// https://docs.astro.build/guides/content-collections/
+
+import { defineCollection } from 'astro:content';
+
+import { glob, file } from 'astro/loaders';
+
+import { z } from 'astro/zod';
+
+const blog = defineCollection({
+    loader: glob({ base: './src/content/blogs', pattern: '**/*.{md,mdx}' }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+    }),
+});
+
+export const collections = { blog };
