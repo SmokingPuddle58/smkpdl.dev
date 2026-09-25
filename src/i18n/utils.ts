65458aea2b0a9eac8c2e9ba200fd41efd103ebf3
SmokingPuddle58 <89989565+SmokingPuddle58@users.noreply.github.com>
2026-09-25 09:45:45 +0800

    chore: Removed testing script

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -3,7 +3,6 @@
 import { ui, defaultLang } from "./ui";
 
 export function getLangFromUrl(url: URL) {
-  console.log(ui)
   const [, lang] = url.pathname.split("/");
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
